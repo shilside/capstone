b4ca5b66889333c4fc026913681b4726f9f3a8a5
shilside <shilohnjoku@gmail.com>
2023-02-09 18:19:41 -0500

    none

--- YCIT099-Capstone/Capstone/server/server.js
+++ YCIT099-Capstone/Capstone/server/server.js
@@ -1,12 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
-  res.json({
-    name: "Josh",
-    age: 23
-  })
-})
+app.get("/", (req, res) => {
+  res.json({});
+});
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
